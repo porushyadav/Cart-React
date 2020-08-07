27205d528f274416ac59feba7691ca44ea0275af
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-08-07 19:42:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeLfzHtpudjlos8AOiyxPIT5rktHG_LXE",
-  authDomain: "cart-4c74f.firebaseapp.com",
-  databaseURL: "https://cart-4c74f.firebaseio.com",
-  projectId: "cart-4c74f",
-  storageBucket: "cart-4c74f.appspot.com",
-  messagingSenderId: "829713431522",
-  appId: "1:829713431522:web:0b44d496fde6bed45417bb"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   
 };
 // Initialize Firebase
